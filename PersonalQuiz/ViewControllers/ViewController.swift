e76d5b20f15ad8c30beb3d1e9ea134ee5286e133
Sergey Zaichkov <sergey.zaichkov@yandex.ru>
2022-11-10 04:34:47 +0100

    personal quiz: ui, model

---
+++ PersonalQuiz/ViewControllers/ViewController.swift
@@ -0,0 +1,13 @@
+
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private let question = Question.getQuestions()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
